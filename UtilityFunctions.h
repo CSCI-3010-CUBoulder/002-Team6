5c2ffe69d53a3c7730da6daeaea5d2408bd7f1fb
Vincent-Zag <acewinner11@gmail.com>
2020-02-11 18:14:53 +0000

    added functions to be implemented

---
+++ UtilityFunctions.h
@@ -0,0 +1,17 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+
+Class Ultility{
+  public:
+  // returns -1 if the number is negative and 1 if positive
+  int Sign(int num);
+  
+  // divides an input integer by 2 until it is impossible to do so, then returns the final number.
+  // (16 = 2 * 2 * 2 * 2 * 1 -> 1, 7 -> 7, 26 = 2 * 13 -> 13, 52 = 2 * 2 * 13 -> 13)
+  int RemoveTwos(int original);
+
+  
+}
+
+#endif
